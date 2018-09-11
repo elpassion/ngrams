e5ab025e5e3f11027b3d7147c340be8b2f484787
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-09-11 22:33:12 +0200

    Update benchmark

--- benchmarks/main.rb
+++ benchmarks/main.rb
@@ -7,21 +7,23 @@ end
 
 require_relative '../lib/ngrams_counter'
 
-text = 'elo co tam co tam'
-
+text = File.read('shakespeare-clean-smallest.txt')
+basic = NGramsCounter.build(:basic)
 tree = NGramsCounter.build(:tree)
-sizes = (1..3)
+sizes = (1..6)
 
 Benchmark.ips do |benchmark|
-  benchmark.report("Tree") do
-    sizes.each do |n|
+  benchmark.config(warmup: 15, time: 25)
+
+  sizes.each do |n|
+    benchmark.report("Basic n=#{n}") do
+      basic.most_frequent(text, n: n)
+    end
+
+    benchmark.report("Tree n=#{n}") do
       tree.most_frequent(text, n: n)
     end
   end
 
-  benchmark.report("Tree") do
-    sizes.each do |n|
-      tree.most_frequent(text, n: n)
-    end
-  end
+  benchmark.compare!
 end
