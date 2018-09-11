3426506e89778cbdb349f38984398e82940ef929
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-09-11 14:23:52 +0200

    Add spec for tokenizer

---
+++ spec/tokenizers/basic_spec.rb
@@ -0,0 +1,13 @@
+require 'ngrams_counter'
+
+RSpec.describe Tokenizers::Basic do
+  let(:text) { "ala ma kot\nkot ma ala ma" }
+
+  describe '#tokenize' do
+    subject { described_class.new.tokenize(text) }
+
+    it 'splits text by whitespaces' do
+      is_expected.to eq(['ala', 'ma', 'kot', 'kot', 'ma', 'ala', 'ma'])
+    end
+  end
+end
